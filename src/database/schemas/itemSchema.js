50b47700de5367d63709dcc4b086ea2e10fe7821
chimnayajith <chinmayajith30@gmail.com>
2024-08-28 13:50:13 +0530

    feat(api): add endpoint to upload files

---
+++ src/database/schemas/itemSchema.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Types.ObjectId,
+      auto: true,
+    },
+    type: {
+      type: String,
+      enum: ["file", "folder"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    format: {
+      type: String,
+      required: function () {
+        return this.type === "file";
+      },
+    },
+    size: {
+      type: String,
+    },
+    total_files: {
+      type: String,
+      required: function () {
+        return this.type === "folder";
+      },
+    },
+    access_control: {
+      type: Array,
+    },
+    owner_id: {
+      type: String,
+      required: function () {
+        return this.type === "folder";
+      },
+    },
+    parent_folder: {
+      type: String,
+      required: false,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Item", itemSchema);
