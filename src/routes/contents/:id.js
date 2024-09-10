0d9dfc75fca9c9ad5e8188ece7e4261f86b6967c
chimnayajith <chinmayajith30@gmail.com>
2024-09-10 14:54:16 +0530

    fix: return empty string and validate objectId

--- src/routes/contents/:id.js
+++ src/routes/contents/:id.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../../database/schemas/itemSchema");
 const verifyToken = require("../../middleware/verifyToken");
+const mongoose = require("mongoose");
 
 router.get("/:id", verifyToken, async (req, res) => {
   const { id } = req.params;
@@ -14,8 +15,13 @@ router.get("/:id", verifyToken, async (req, res) => {
         owner_id: req.userId,
       });
     } else {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid folder ID format" });
+      }
+
+
       const folder = await Item.countDocuments({
-        _id: id,
+        _id: new mongoose.Types.ObjectId(id),
         owner_id: req.userId,
         type: "folder",
       });
@@ -28,10 +34,6 @@ router.get("/:id", verifyToken, async (req, res) => {
         parent_folder: id,
         owner_id: req.userId,
       });
-    }
-
-    if (!content.length) {
-      return res.status(400).send({ message: "No content found" });
     }
 
     res.send(content);
