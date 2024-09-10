94c6979c22ebfec0c397d7f33d32cfa795e70d0a
chimnayajith <chinmayajith30@gmail.com>
2024-09-10 11:09:56 +0530

    feat: change root content endpoint

--- src/routes/contents/:id.js
+++ src/routes/contents/:id.js
@@ -6,10 +6,20 @@ const verifyToken = require("../../middleware/verifyToken");
 router.get("/:id", verifyToken, async (req, res) => {
   const { id } = req.params;
   try {
-    const content = await Item.find({
-      parent_folder: id,
-      owner_id: req.userId,
-    });
+    let content;
+
+    if ( id === "home"){
+      content = await Item.find({ parent_folder:"null",owner_id: req.userId });
+    } else {
+      content = await Item.find({
+        parent_folder: id,
+        owner_id: req.userId,
+      });
+    }
+
+    if (!content.length) {
+      return res.status(404).send({ message: "No content found" });
+    }
 
     res.send(content);
   } catch (error) {
