6ca47ea57ba8445e94d30036c61431c70f6977f4
chimnayajith <chinmayajith30@gmail.com>
2024-09-16 11:42:28 +0530

    chore: fix linting issues

--- src/routes/files/fetch.js
+++ src/routes/files/fetch.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { S3Client, ListObjectVersionsCommand } = require("@aws-sdk/client-s3");
-const verifyToken = require("../../middleware/verifyToken");
 
 const s3Client = new S3Client({
   region: "us-east-005",
