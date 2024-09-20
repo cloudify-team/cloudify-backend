1d0e98b8bfdcb721f624772cc0d4988e63ee6926
chimnayajith <chinmayajith30@gmail.com>
2024-09-20 16:16:51 +0530

    feat: single s3 client for better performance

--- src/routes/files/fetch.js
+++ src/routes/files/fetch.js
@@ -1,14 +1,6 @@
 const express = require("express");
-const { S3Client, ListObjectVersionsCommand } = require("@aws-sdk/client-s3");
-
-const s3Client = new S3Client({
-  region: "us-east-005",
-  endpoint: "https://s3.us-east-005.backblazeb2.com",
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  },
-});
+const { ListObjectVersionsCommand } = require("@aws-sdk/client-s3");
+const s3Client = require("../../utils/s3Client");
 
 const router = express.Router();
 
