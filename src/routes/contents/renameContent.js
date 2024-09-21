668ecfbd9fcc990d682354266fbf8b30f53c2dd1
chimnayajith <chinmayajith30@gmail.com>
2024-09-21 15:58:49 +0530

    feat: rename item endpoint

---
+++ src/routes/contents/renameContent.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const verifyToken = require("../../middleware/verifyToken");
+const { renameItem } = require("../../utils/renameItem");
+const Item = require("../../database/schemas/itemSchema");
+
+const router = express.Router();
+
+router.put("/rename", verifyToken, async (req, res) => {
+  const { _id, newName } = req.body;
+
+  const errors = [];
+
+  if (!_id) {
+    errors.push({ field: "email", error: "Email is required." });
+  }
+  if (!newName) {
+    errors.push({ field: "password", error: "Password is required." });
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ success: false, errors });
+  }
+
+  try {
+    const item = await Item.findById(new mongoose.Types.ObjectId(_id));
+
+    if (!item) {
+      return res.status(400).send({
+        success: false,
+        errors: [{ field: "_id", error: "Invalid _id provided" }],
+      });
+    }
+    if (item.name === newName) {
+      return res.status(400).json({
+        message: "The new name is the same as the current name.",
+      });
+    }
+
+    await renameItem(item, newName);
+    return res.status(200).send({
+      success: true,
+      message: `Item renamed succesfully.`,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = router;
